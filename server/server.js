6f7e8afc44c4d459fe8fa4aef3e9f359a30b2269
unknown <vogel-julian@web.de>
2021-03-04 13:06:41 +0100

    mongoDb connection

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const mongoose = require('mongoose');
     })
 
     try{
-        await mongoose.connect(mongoURI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
